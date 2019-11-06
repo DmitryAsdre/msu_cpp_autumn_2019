9861f2c543314c0a760948056d394ec678f9dfd6
dmitry <michalych2014@yandex.ru>
2019-11-06 23:54:38 +0300

    all

--- 04/BigInt.hpp
+++ 04/BigInt.hpp
@@ -12,6 +12,39 @@ class BigInt
         BigInt(const Vector& v, int sign);
         BigInt(const std::string& str);
 
+        BigInt& operator -();
+
+        BigInt operator +(const BigInt& add)const;
+        BigInt operator -(const BigInt& minus)const;
+
+        BigInt& operator +=(const BigInt& add);
+        BigInt& operator -=(const BigInt& add);
+
+        BigInt operator +(int add)const;
+        BigInt operator -(int add)const;
+
+        BigInt& operator +=(int add);
+        BigInt& operator -=(int add);
+
+        
+        bool operator <(const BigInt& cmp)const;
+        bool operator >(const BigInt& cmp)const;
+        bool operator ==(const BigInt& cmp)const;
+        bool operator !=(const BigInt& cmp)const;
+        bool operator <=(const BigInt& cmp)const;
+        bool operator >=(const BigInt& cmp)const;
+
+        bool operator <(int cmp)const;
+        bool operator >(int cmp)const;
+        bool operator ==(int cmp)const;
+        bool operator !=(int cmp)const;
+        bool operator <=(int cmp)const;
+        bool operator >=(int cmp)const;
+
+        BigInt& operator =(const BigInt& tmp);
+        BigInt& operator =(int tmp);
+        BigInt& operator =(const std::string& str);
+
         std::string toString()const;
 
 };
